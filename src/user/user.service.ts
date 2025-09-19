2646492bc1d4d4bc1a45b331e01390b0b86e9436
darwinz10 <kyosukekanou88@gmail.com>
2025-09-19 16:58:36 -0500

    just test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -175,7 +175,7 @@ export class UserService {
     const token = this.jwtService.sign(infoUser);
 
     response.cookie("token",token,{
-      httpOnly:false,
+      httpOnly:true,
       secure:true,
       sameSite:'none',
       maxAge:3600*1000
@@ -188,7 +188,7 @@ export class UserService {
    logout(response:Response){
      
     response.clearCookie("token",{
-      httpOnly:false,
+      httpOnly:true,
       secure:true,
       sameSite:'none'
     });
